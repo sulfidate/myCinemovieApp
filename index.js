36298c2230606c7574443700e58f4da40bd70c83
sulfidate <ffluid@me.com>
2021-07-04 20:20:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ const app = express();
 
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://mycinemoviedatabase.herokuapp.com'];
-
 app.use(cors({
 	origin: (origin, callback) => {
 		if(!origin) return callback(null, true);
@@ -41,6 +39,7 @@ app.use(methodOverride());
 
 app.use(express.static('public'));
 
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://mycinemoviedatabase.herokuapp.com'];
 
 let auth = require('./auth')(app);
 
