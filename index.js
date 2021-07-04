8fbec1b7b42f33cac815b2f48f81065ad3f722c2
sulfidate <ffluid@me.com>
2021-07-04 19:04:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,17 +18,16 @@ const express = require('express'),
 
 const app = express();
 
-app.use(cors());
-// app.use(cors({
-// 	origin: (origin, callback) => {
-// 		if(!origin) return callback(null, true);
-// 		if(allowedOrigins.indexOf(origin) === -1){ 
-// 			let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
-// 			return callback(new Error(message ), false); 
-// 		}
-// 		return callback(null, true);
-// 	}
-// }));
+app.use(cors({
+	origin: (origin, callback) => {
+		if(!origin) return callback(null, true);
+		if(allowedOrigins.indexOf(origin) === -1){ 
+			let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+			return callback(new Error(message ), false); 
+		}
+		return callback(null, true);
+	}
+}));
 
 const { check, validationResult } = require('express-validator');
 
