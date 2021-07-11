3ce612700a658a93de09b1321e09d95981fd8712
sulfidate <ffluid@me.com>
2021-07-11 16:26:49 +0200

    search error

--- auth.js
+++ auth.js
@@ -22,7 +22,8 @@ module.exports = (router) => {
 			if (error || !user) {
 				return res.status(400).json({
 					message: 'Something is not right',
-					user: user
+					user: user,
+					console.log(error, user, info)
 				});
 			}
 			req.login(user, { session: false }, (error) => {
