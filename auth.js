c93944e67d18b8b4f1256c8dbe73cab45fea157e
sulfidate <ffluid@me.com>
2021-07-12 13:18:04 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -22,9 +22,9 @@ module.exports = (router) => {
 			if (error || !user) {
 				return res.status(400).json({
 					message: 'Something is not right',
-					user: user
+					user: user,
+					console.log(user, info, error);
 				});
-				console.log(user, info, error);
 
 			}
 			req.login(user, { session: false }, (error) => {
