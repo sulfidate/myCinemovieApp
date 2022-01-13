51cc19e2022a5a96b83ffa042a572a28a4ba9e60
sulfidate <ffluid@me.com>
2022-01-13 10:02:00 +0000

    index

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(methodOverride());
 
 app.use(express.static('public'));
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://mycinemoviedatabase.herokuapp.com', * ];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://mycinemoviedatabase.herokuapp.com' ];
 
 app.use(bodyParser.json());
 
