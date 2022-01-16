23f0cd9c65af6b6232fa9d6af5c31ba33bbddd79
sulfidate <ffluid@me.com>
2022-01-16 14:56:44 +0000

    update index.js

--- index.js
+++ index.js
@@ -211,26 +211,26 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false }),
 });
 
 // Get all Movies
-app.get("/movies", function (req, res) {
-	Movies.find()
-		.then(function (movies) {
-			res.status(201).json(movies);
-		})
-		.catch(function (error) {
-			console.error(error);
-			res.status(500).send("Error: " + error);
-		});
-});
-// app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+// app.get("/movies", function (req, res) {
 // 	Movies.find()
-// 		.then((movies) => {
+// 		.then(function (movies) {
 // 			res.status(201).json(movies);
 // 		})
-// 		.catch((error) => {
+// 		.catch(function (error) {
 // 			console.error(error);
-// 			res.status(500).send('Error: ' + error);
+// 			res.status(500).send("Error: " + error);
 // 		});
 // });
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+	Movies.find()
+		.then((movies) => {
+			res.status(201).json(movies);
+		})
+		.catch((error) => {
+			console.error(error);
+			res.status(500).send('Error: ' + error);
+		});
+});
 
 // app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
 // 	Movies.find()
