44bebc03a119b63257ee5242ae433b61fbfaa5f8
sulfidate <ffluid@me.com>
2022-01-22 17:31:39 +0000

    password validation  update

--- passport.js
+++ passport.js
@@ -20,8 +20,13 @@ Users.findOne({ Username: username }, (error, user) => {
 
 	if (!user) {
 		console.log('incorrect username');
-		return callback(null, false, {message: 'Incorrect username or password.'});
+		return callback(null, false, {message: 'Incorrect username.'});
 	}
+
+if (!user.validatePassword(password)) {
+	console.log('incorrect password');
+	return callback(null, false, {message: 'Incorrect password.'});
+}
 
 	console.log('finished');
 	return callback(null, user);
