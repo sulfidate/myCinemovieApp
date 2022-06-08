b32fa45603b09c289e5c6875c7557378e3183b3a
sulfidate <ffluid@me.com>
2022-06-08 15:36:43 +0000

    fix hashed Password

--- index.js
+++ index.js
@@ -122,16 +122,16 @@ app.put('/users/:Username',
 	
 	passport.authenticate('jwt', { session: false }), (req, res) => {
 		let errors = validationResult(req);
-		let hashedPassword = Users.hashPassword(req.body.Password);
+		
 		if (!errors.isEmpty()) {
 			return res.status(422).json({ errors: errors.array() });
 		}
 		
-		
+	let hashedPassword = Users.hashPassword(req.body.Password);	
 	Users.findOneAndUpdate({ Username: req.params.Username }, { $set: 
 		{
 			Username: req.body.Username,
-			Password: req.body.Password,
+			Password: hashedPassword,
 			Email: req.body.Email,
 			Birthday: req.body.Birthday
 		}
